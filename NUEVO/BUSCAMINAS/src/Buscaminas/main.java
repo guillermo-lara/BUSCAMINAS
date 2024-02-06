5341f8054c26bee9847519b5b13fde0ba9673256
guillermo-lara <114028519+guillermo-lara@users.noreply.github.com>
2024-02-06 11:10:21 +0100

    Add files via upload

---
+++ NUEVO/BUSCAMINAS/src/Buscaminas/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Buscaminas;
+import Interfaces.*;
+import Clases.*;
+import java.io.File;
+
+public class main 
+{
+    
+    public static File f = new File("C:\\BUSCAMINAS\\note");
+
+    public static void main(String[] args)
+    {
+        InicioSesion Comienzo = new InicioSesion();
+    }
+    
+}
---
+++ NUEVO/BUSCAMINAS/src/Clases/Archivo.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import Buscaminas.*;
+import Interfaces.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Archivo
+{
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
