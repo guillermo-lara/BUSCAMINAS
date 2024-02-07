d3efcc0debe579791f095136c92f07db4caf26af
guillermo-lara <g.lara.2022@alumnos.urjc.es>
2024-02-07 12:30:14 +0100

    CAMBIOS EN ARCHIVO, INICIO SESION Y JUGADOR

--- NUEVO/BUSCAMINAS/src/Interfaces/InicioSesion.java
+++ NUEVO/BUSCAMINAS/src/Interfaces/InicioSesion.java
@@ -4,6 +4,7 @@
  */
 package Interfaces;
 
+import Clases.Archivo;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -25,12 +26,92 @@ public class InicioSesion extends javax.swing.JFrame
 
     public InicioSesion()
     {
-        
         initComponents();
-        File f = Buscaminas.main.f;
     }
-   
+    int ln = Buscaminas.main.archivo.ln; 
+
+
+
+
+    void getJugador(String name,String psw,String Usuario,String Contrasena)
+    {
+        try
+        {
+            RandomAccessFile raf = new RandomAccessFile(Buscaminas.main.archivo.fJ,"rw");
+            for(int i =0; i<(ln-1); i+=8){
+                Usuario = raf.readLine().substring(8);
+                Contrasena = raf.readLine().substring(11);
+                if(name.equals(Usuario)& psw.equals(Contrasena))
+                {
+                    int aux = i/8;
+                   
+                    Interfaces.Jugador mJUGADOR = new Interfaces.Jugador(aux);
+                    mJUGADOR.setVisible(true);
+                    this.setVisible(false);
+                    break;
+                }else if (i==(ln-8)-i/8)
+                {
+                   JOptionPane.showMessageDialog(null, "Usuario/Contraseña erroneo");
+                   break;
+                }
+                for(int k=1; k<=7;k++)
+                {
+                    raf.readLine();
+                }
+            }
+        } catch (FileNotFoundException ex)
+        {
+            Logger.getLogger(Archivo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex)
+        {
+            Logger.getLogger(Archivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
     
+    
+    
+    void getAdmin(String name,String psw,String Usuario,String Contrasena)
+    {
+        try
+        {
+            RandomAccessFile raf = new RandomAccessFile(Buscaminas.main.archivo.fA,"rw");
+            for(int i =0; i<(ln-1); i+=3){
+                Usuario = raf.readLine().substring(8);
+                Contrasena = raf.readLine().substring(11);
+                if(name.equals(Usuario) && psw.equals(Contrasena))
+                {
+                    Interfaces.Administrador mADMIN = new Interfaces.Administrador();
+                    mADMIN.setVisible(true);
+                    this.setVisible(false);
+                    break;
+                }if(i==(ln-3)-i/3){
+                JOptionPane.showMessageDialog(null, "Usuario/Contraseña erroneo");
+                break;
+                }
+                for(int k=1; k<=2;k++)
+                {
+                    raf.readLine();
+                }
+            }
+        } catch (FileNotFoundException ex)
+        {
+            Logger.getLogger(Archivo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex)
+        {
+            Logger.getLogger(Archivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+
+
+
+
+
+
+
+
+
+
+
     
 
 
@@ -56,6 +137,18 @@ public class InicioSesion extends javax.swing.JFrame
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel3.setText("CONTRASEÑA:");
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jPasswordField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jPasswordField1ActionPerformed(evt);
+            }
+        });
 
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -121,23 +214,33 @@ public class InicioSesion extends javax.swing.JFrame
         }
         else if(option.equals("JUGADOR"))
         {
-          /* 
-            crearCarpeta();
-            numeroLineas("C:\\BUSCAMINAS\\note\\loginsJ.txt");
+
+            
             getJugador(name,psw);
-           */ 
         }
         else if(option.equals("ADMIN"))
         {
-            /*
-            crearCarpeta();
-            numeroLineas("C:\\BUSCAMINAS\\note\\loginsA.txt");
+            
             getAdmin(name,psw);
-            */
+            
         }
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+
+
+
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+
+
+    }//GEN-LAST:event_jPasswordField1ActionPerformed
+
+    
+    
+    
     /**
      * @param args the command line arguments
      */
